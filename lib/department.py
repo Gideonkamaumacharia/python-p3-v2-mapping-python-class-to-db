7f9b7280a68003e978fcb78b32a6e262a2641901
Gideon Macharia <kamaugedion95@gmail.com>
2024-06-07 17:23:00 +0300

    final commit

--- lib/department.py
+++ lib/department.py
@@ -1,8 +1,6 @@
 from __init__ import CURSOR, CONN
 
-
 class Department:
-
     def __init__(self, name, location, id=None):
         self.id = id
         self.name = name
@@ -10,3 +8,60 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of Department instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            location TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persists Department instances """
+        sql = """
+            DROP TABLE IF EXISTS departments;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+    
+    def save(self):
+        sql = """
+            INSERT INTO departments (name,location) VALUES (?,?)
+        """
+        params = (self.name,self.location)
+        CURSOR.execute(sql,params)
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+    
+    @classmethod
+    def create(cls,name,location):
+        department = Department(name,location)
+        department.save()
+        return department
+    
+    def update(self):
+        sql = """
+            UPDATE departments 
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        params = (self.name,self.location,self.id)
+        CURSOR.execute(sql,params)
+        CONN.commit()
+    
+    def delete(self):
+        sql = """
+            DELETE FROM departments
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql,(self.id,))
+        CONN.commit()
